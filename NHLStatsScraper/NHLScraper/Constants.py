0085ef30cd9db29aca64784d74e7bf9e78dd1404
jacobyam <jacobyamrhit@gmail.com>
2019-10-14 00:36:49 -0700

    Parses DFO lineup info

---
+++ NHLStatsScraper/NHLScraper/Constants.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 14, 2019
+
+@author: Elite-Intel
+'''
+
+class DFO(object):
+    '''
+    classdocs
+    '''
+    ROSTER_URL = "https://www.dailyfaceoff.com/teams/{}/line-combinations/"
+    FORWARD_ABBREV = "f"
+    MAX_LINES = 4
+    PLAYER_NAME = "player-name"
+        
+
+
+        
--- NHLStatsScraper/NHLScraper/Scraper.py
+++ NHLStatsScraper/NHLScraper/Scraper.py
@@ -2,8 +2,28 @@
 Created on Oct 13, 2019
 
 @author: Elite-Intel
+
 '''
+import requests
+from bs4 import BeautifulSoup
+import Constants
+
+DFO = Constants.DFO()
+
+def getDFORoster(teamName):
+    url = DFO.ROSTER_URL.format(teamName)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    roster = []
+    for i in range(1, DFO.MAX_LINES+1):
+        line = DFO.FORWARD_ABBREV + str(i)
+        forwards = soup.find(id=line)
+        players = forwards.find_all(class_= DFO.PLAYER_NAME)
+        lineArr = []
+        for player in players:
+            lineArr.append(player.get_text().strip())
+        roster.append(lineArr)
+    return roster
 
 if __name__ == '__main__':
-    print "Hello World"
     pass
