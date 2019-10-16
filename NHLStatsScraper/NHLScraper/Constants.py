f83c17969bf482056f4e5284743ac9c79de5dba4
jacobyam <jacobyamrhit@gmail.com>
2019-10-16 02:22:29 -0700

    Now gets the player info table from Natural Stat Trick

--- NHLStatsScraper/NHLScraper/Constants.py
+++ NHLStatsScraper/NHLScraper/Constants.py
@@ -4,7 +4,13 @@ Created on Oct 14, 2019
 @author: Elite-Intel
 '''
 
-class DFO(object):
+class NST():
+    TEAM_PAGE_URL = 'https://www.naturalstattrick.com/linestats.php?fromseason=20192020&thruseason=20192020&stype=2&sit=5v5&score=all&rate=n&team={}&vteam=ALL&view=wowy&loc=B&gpfilt=none&fd=2019-10-02&td=2020-04-04&tgp=2000&strict=incl'
+    LINE_PAGE_URL = 'https://www.naturalstattrick.com/linestats.php?fromseason=20192020&thruseason=20192020&stype=2&sit=5v5&score=all&rate=n&team=ANA&vteam=ALL&view=wowy&loc=B&gpfilt=none&fd=2019-10-02&td=2020-04-04&tgp=2000&strict=incl&p1={}&p2={}&p3={}&p4=0&p5=0'
+    PLAYER_ABBREV = 'p1'
+    DEFAULT_PLAYER_VAL = 0
+    
+class DFO():
     '''
     classdocs
     '''
--- NHLStatsScraper/NHLScraper/Scraper.py
+++ NHLStatsScraper/NHLScraper/Scraper.py
@@ -9,7 +9,34 @@ from bs4 import BeautifulSoup
 import Constants
 
 DFO = Constants.DFO()
+NST = Constants.NST()
 
+
+def getNSTTeamPageSoup(teamAbbreviation):
+    url = NST.TEAM_PAGE_URL.format(teamAbbreviation)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+
+def getNSTPlayerID(soup,name):
+    playerOptions = soup.find('select',{'name':NST.PLAYER_ABBREV})
+    for option in playerOptions.find_all('option'):
+        if(option.text == name):
+            return option['value']
+    return NST.DEFAULT_PLAYER_VAL
+
+def getNSTLineStats(soup,line):
+    p1 = getNSTPlayerID(soup,line[0])
+    p2 = getNSTPlayerID(soup,line[1])    
+    p3 = getNSTPlayerID(soup,line[2])
+    url = NST.LINE_PAGE_URL.format(p1,p2,p3)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,'html.parser')
+    table = soup.find('table', id='players')
+    print table
+    
+
+    
 def getDFORoster(teamName):
     url = DFO.ROSTER_URL.format(teamName)
     page = requests.get(url)
@@ -26,4 +53,7 @@ def getDFORoster(teamName):
     return roster
 
 if __name__ == '__main__':
+    roster = getDFORoster("anaheim-ducks")
+    soup = getNSTTeamPageSoup('ana')
+    getNSTLineStats(soup,roster[0])
     pass
