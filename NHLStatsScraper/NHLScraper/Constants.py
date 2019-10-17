c588d400646743306e19c860159a460dbbbe06c6
jacobyam <jacobyamrhit@gmail.com>
2019-10-17 01:55:36 -0700

    Now writes stats+header for one team to file

--- NHLStatsScraper/NHLScraper/Constants.py
+++ NHLStatsScraper/NHLScraper/Constants.py
@@ -9,6 +9,7 @@ class NST():
     LINE_PAGE_URL = 'https://www.naturalstattrick.com/linestats.php?fromseason=20192020&thruseason=20192020&stype=2&sit=5v5&score=all&rate=n&team=ANA&vteam=ALL&view=wowy&loc=B&gpfilt=none&fd=2019-10-02&td=2020-04-04&tgp=2000&strict=incl&p1={}&p2={}&p3={}&p4=0&p5=0'
     PLAYER_ABBREV = 'p1'
     DEFAULT_PLAYER_VAL = 0
+    DEFAULT_STATS_HEADERS = ['Player 1', 'Player 2', 'Player 3', 'GP', 'TOI', 'CF', 'CA', 'CF%', 'FF', 'FA', 'FF%', 'SF', 'SA', 'SF%', 'GF', 'GA', 'GF%', 'xGF', 'xGA', 'xGF%', 'SCF', 'SCA', 'SCF%', 'HDCF', 'HDCA', 'HDCF%', 'HDGF', 'HDGA', 'HDGF%', 'MDCF', 'MDCA', 'MDCF%', 'MDGF', 'MDGA', 'MDGF%', 'LDCF', 'LDCA', 'LDCF%', 'LDGF', 'LDGA', 'LDGF%', 'On-Ice SH%', 'On-Ice SV%', 'PDO', 'Off.\xa0Zone Faceoffs', 'Neu.\xa0Zone Faceoffs', 'Def.\xa0Zone Faceoffs', 'Off.\xa0Zone Faceoff %']
     
 class DFO():
     '''
--- NHLStatsScraper/NHLScraper/Scraper.py
+++ NHLStatsScraper/NHLScraper/Scraper.py
@@ -7,6 +7,8 @@ Created on Oct 13, 2019
 import requests
 from bs4 import BeautifulSoup
 import Constants
+import csv
+
 
 DFO = Constants.DFO()
 NST = Constants.NST()
@@ -25,16 +27,24 @@ def getNSTPlayerID(soup,name):
             return option['value']
     return NST.DEFAULT_PLAYER_VAL
 
+def getNumSearchablePlayers(p1,p2,p3):
+    return (1 if(p1 != 0) else 0) + (1 if(p2 != 0) else 0) + (1 if(p3 != 0) else 0)
+
 def getNSTLineStats(soup,line):
     p1 = getNSTPlayerID(soup,line[0])
     p2 = getNSTPlayerID(soup,line[1])    
     p3 = getNSTPlayerID(soup,line[2])
+    numPlayers = getNumSearchablePlayers(p1,p2,p3)
     url = NST.LINE_PAGE_URL.format(p1,p2,p3)
     page = requests.get(url)
     soup = BeautifulSoup(page.content,'html.parser')
     table = soup.find('table', id='players')
-    print table
-    
+    rows = table.find_all('tr')
+    fullLineTDs= rows[1].find_all('td')
+    lineData = []
+    for td in fullLineTDs:
+        lineData.append(td.get_text())
+    return lineData[numPlayers:len(lineData)]    
 
     
 def getDFORoster(teamName):
@@ -52,8 +62,21 @@ def getDFORoster(teamName):
         roster.append(lineArr)
     return roster
 
+def getRosterStats(soup,roster):
+    rosterWithStats = []
+    for line in roster:
+        rosterWithStats.append(line + getNSTLineStats(soup,line))
+    return rosterWithStats
+
+def writeToFile(roster,filename='output.csv'):
+    with open(filename,'wb') as result_file:
+        wr = csv.writer(result_file, dialect='excel')
+        wr.writerow(NST.DEFAULT_STATS_HEADERS)
+        wr.writerows(roster)
+
 if __name__ == '__main__':
     roster = getDFORoster("anaheim-ducks")
     soup = getNSTTeamPageSoup('ana')
-    getNSTLineStats(soup,roster[0])
+    stats = getRosterStats(soup,roster)
+    writeToFile(stats)
     pass
