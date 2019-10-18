a50cb8b18dcf8306d9d7bebcd3629ed5553284f1
jacobyam <jacobyamrhit@gmail.com>
2019-10-18 02:20:38 -0700

    Now generates csv for all matches, team dict is incomplete and code is very messy

--- NHLStatsScraper/NHLScraper/Constants.py
+++ NHLStatsScraper/NHLScraper/Constants.py
@@ -3,10 +3,30 @@ Created on Oct 14, 2019
 
 @author: Elite-Intel
 '''
+class NHL():
+    SCHEDULE_API_URL = 'https://statsapi.web.nhl.com/api/v1/schedule'
+    HOME = 'home'
+    AWAY = 'away'
+    NAME = 'name'
+    ROSTER = 'ROSTER'
+    TEAM_NAMES = {
+                    'Anaheim Ducks':{'DFO':'Anaheim-Ducks','NST':'ANA'},
+                    'Carolina Hurricanes':{'DFO':'Carolina-Hurricanes','NST':'CAR'},
+                    'Chicago Blackhawks':{'DFO':'chicago-blackhawks','NST':'CHI'},
+                    'Colorado Avalanche':{'DFO':'colorado-avalanche','NST':'COL'},
+                    'Columbus Blue Jackets':{'DFO':'columbus-blue-jackets','NST':'CBJ'},
+                    'Dallas Stars':{'DFO':'dallas-stars','NST':'DAL'},
+                    'Detroit Red Wings':{'DFO':'Detroit-Red-Wings','NST':'DET'},
+                    'Edmonton Oilers':{'DFO':'Edmonton-Oilers','NST':'EDM'},
+                    'Florida Panthers':{'DFO':'florida-panthers','NST':'FLA'},
+                    'New York Rangers':{'DFO':'new-york-rangers','NST':'NYR'},
+                    'Pittsburgh Penguins':{'DFO':'pittsburgh-penguins','NST':'PIT'},
+                    'Washington Capitals':{'DFO':'washington-capitals','NST':'WSH'},
+                 }
 
 class NST():
     TEAM_PAGE_URL = 'https://www.naturalstattrick.com/linestats.php?fromseason=20192020&thruseason=20192020&stype=2&sit=5v5&score=all&rate=n&team={}&vteam=ALL&view=wowy&loc=B&gpfilt=none&fd=2019-10-02&td=2020-04-04&tgp=2000&strict=incl'
-    LINE_PAGE_URL = 'https://www.naturalstattrick.com/linestats.php?fromseason=20192020&thruseason=20192020&stype=2&sit=5v5&score=all&rate=n&team=ANA&vteam=ALL&view=wowy&loc=B&gpfilt=none&fd=2019-10-02&td=2020-04-04&tgp=2000&strict=incl&p1={}&p2={}&p3={}&p4=0&p5=0'
+    LINE_PAGE_URL = 'https://www.naturalstattrick.com/linestats.php?fromseason=20192020&thruseason=20192020&stype=2&sit=5v5&score=all&rate=n&team={}&vteam=ALL&view=wowy&loc=B&gpfilt=none&fd=2019-10-02&td=2020-04-04&tgp=2000&strict=incl&p1={}&p2={}&p3={}&p4=0&p5=0'
     PLAYER_ABBREV = 'p1'
     DEFAULT_PLAYER_VAL = 0
     DEFAULT_STATS_HEADERS = ['Player 1', 'Player 2', 'Player 3', 'GP', 'TOI', 'CF', 'CA', 'CF%', 'FF', 'FA', 'FF%', 'SF', 'SA', 'SF%', 'GF', 'GA', 'GF%', 'xGF', 'xGA', 'xGF%', 'SCF', 'SCA', 'SCF%', 'HDCF', 'HDCA', 'HDCF%', 'HDGF', 'HDGA', 'HDGF%', 'MDCF', 'MDCA', 'MDCF%', 'MDGF', 'MDGA', 'MDGF%', 'LDCF', 'LDCA', 'LDCF%', 'LDGF', 'LDGA', 'LDGF%', 'On-Ice SH%', 'On-Ice SV%', 'PDO', 'Off.\xa0Zone Faceoffs', 'Neu.\xa0Zone Faceoffs', 'Def.\xa0Zone Faceoffs', 'Off.\xa0Zone Faceoff %']
--- NHLStatsScraper/NHLScraper/Scraper.py
+++ NHLStatsScraper/NHLScraper/Scraper.py
@@ -4,19 +4,30 @@ Created on Oct 13, 2019
 @author: Elite-Intel
 
 '''
-import requests
-from bs4 import BeautifulSoup
-import Constants
 import csv
+import datetime
+import os
+import time
+from bs4 import BeautifulSoup
+import requests
+import Constants
+from random import randint
+
 
 
 DFO = Constants.DFO()
 NST = Constants.NST()
+NHL = Constants.NHL()
 
+def getRequestWithSleep(URL,PARAMS=[]):
+    page = requests.get(url=URL,params = PARAMS)
+    time.sleep(randint(1,10))
+    return page
+    
 
 def getNSTTeamPageSoup(teamAbbreviation):
     url = NST.TEAM_PAGE_URL.format(teamAbbreviation)
-    page = requests.get(url)
+    page = getRequestWithSleep(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     return soup
 
@@ -30,13 +41,16 @@ def getNSTPlayerID(soup,name):
 def getNumSearchablePlayers(p1,p2,p3):
     return (1 if(p1 != 0) else 0) + (1 if(p2 != 0) else 0) + (1 if(p3 != 0) else 0)
 
-def getNSTLineStats(soup,line):
+def getNSTLineStats(line,abbr):
+    soup = getNSTTeamPageSoup(abbr)
     p1 = getNSTPlayerID(soup,line[0])
     p2 = getNSTPlayerID(soup,line[1])    
     p3 = getNSTPlayerID(soup,line[2])
     numPlayers = getNumSearchablePlayers(p1,p2,p3)
-    url = NST.LINE_PAGE_URL.format(p1,p2,p3)
-    page = requests.get(url)
+    if numPlayers <= 1:
+        return ['Not enough searchable players to return data!']
+    url = NST.LINE_PAGE_URL.format(abbr,p1,p2,p3)
+    page = getRequestWithSleep(url)
     soup = BeautifulSoup(page.content,'html.parser')
     table = soup.find('table', id='players')
     rows = table.find_all('tr')
@@ -45,11 +59,10 @@ def getNSTLineStats(soup,line):
     for td in fullLineTDs:
         lineData.append(td.get_text())
     return lineData[numPlayers:len(lineData)]    
-
     
 def getDFORoster(teamName):
     url = DFO.ROSTER_URL.format(teamName)
-    page = requests.get(url)
+    page = getRequestWithSleep(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     roster = []
     for i in range(1, DFO.MAX_LINES+1):
@@ -58,25 +71,60 @@ def getDFORoster(teamName):
         players = forwards.find_all(class_= DFO.PLAYER_NAME)
         lineArr = []
         for player in players:
-            lineArr.append(player.get_text().strip())
+            lineArr.append(player.get_text().encode('utf-8').strip())
         roster.append(lineArr)
     return roster
 
-def getRosterStats(soup,roster):
+def getRosterStats(roster,abbr):
     rosterWithStats = []
     for line in roster:
-        rosterWithStats.append(line + getNSTLineStats(soup,line))
+        rosterWithStats.append(line + getNSTLineStats(line,abbr))
     return rosterWithStats
 
-def writeToFile(roster,filename='output.csv'):
-    with open(filename,'wb') as result_file:
+def writeToFile(game,filename='output.csv'):
+    gameTitle = "{}@{}".format(game[NHL.AWAY]['name'],game[NHL.HOME]['name'])
+    append_write = 'ab' if os.path.exists(filename) else 'wb'
+    with open(filename,append_write) as result_file:
         wr = csv.writer(result_file, dialect='excel')
+        wr.writerow([gameTitle])
         wr.writerow(NST.DEFAULT_STATS_HEADERS)
-        wr.writerows(roster)
+        wr.writerows(game[NHL.AWAY][NHL.ROSTER])
+        wr.writerow(NST.DEFAULT_STATS_HEADERS)
+        wr.writerows(game[NHL.HOME][NHL.ROSTER])
+        wr.writerow([])
+
+def getSchedule(date = datetime.datetime.today()):
+    dateString = date.strftime('%Y-%m-%d') 
+    URL = NHL.SCHEDULE_API_URL
+    PARAMS = {'date':dateString}
+    r = getRequestWithSleep(URL, PARAMS)
+    data = r.json()
+    matches = []
+    for game in data['dates'][0]['games']:
+        homeTeam = game['teams']['home']['team'][NHL.NAME]
+        awayTeam = game['teams']['away']['team'][NHL.NAME]
+        matches.append({NHL.HOME:homeTeam, NHL.AWAY:awayTeam})
+    return matches
+
+def getNSTTeamName(nhlName):
+    return NHL.TEAM_NAMES[nhlName]['NST']
+
+def getDFOTeamName(nhlName):
+    return NHL.TEAM_NAMES[nhlName]['DFO']
+
+def getLineStatsForGame(game):
+    gameStats = {}
+    homeNSTName = getNSTTeamName(game[NHL.HOME])
+    awayNSTName = getNSTTeamName(game[NHL.AWAY])
+    homeRoster = getDFORoster(getDFOTeamName(game[NHL.HOME]))
+    awayRoster = getDFORoster(getDFOTeamName(game[NHL.AWAY]))
+    gameStats[NHL.HOME] = {NHL.NAME:homeNSTName,NHL.ROSTER:getRosterStats(homeRoster,homeNSTName)}
+    gameStats[NHL.AWAY] = {NHL.NAME:awayNSTName,NHL.ROSTER:getRosterStats(awayRoster,awayNSTName)}
+    return gameStats
 
 if __name__ == '__main__':
-    roster = getDFORoster("anaheim-ducks")
-    soup = getNSTTeamPageSoup('ana')
-    stats = getRosterStats(soup,roster)
-    writeToFile(stats)
+    games = getSchedule(datetime.datetime(2019,10,18))
+    for game in games:
+        stats = getLineStatsForGame(game)
+        writeToFile(stats)
     pass
